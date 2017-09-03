e6b34f3a7b81652fcf6064c9d2455029ff51c49c
bimaxc <zerobima792@gmail.com>
2017-09-03 20:24:08 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -3,14 +3,14 @@
 
 echo -e "             __________________________________________________"| lolcat
 echo -e "            |                                                  |"| lolcat
-echo -e "            |          SELAMAT DATANG DI VPS $HOSTNAME         |"| lolcat
-echo -e "            |  Admin :                  Admin : Bima S.        |"| lolcat
+echo -e "            |             B-Net Script Ver0.5                  |"| lolcat
+echo -e "            |  Admin :                  Admin : Bima S         |"| lolcat
 echo -e "            |  Admin :                  Admin :                |"| lolcat
 echo -e "            |  Admin :                  Admin :                |"| lolcat
 echo -e "            |__________________________________________________|"| lolcat
 echo -e ""
 echo -e "1.)menu (Menampilkan Daftar Perintah)   7.)resvis (Restart Service Dropbear)"| lolcat
-echo -e "2.)usernew (Membuat Akun SSH & OpenVPN) 8.)info (Menampilkan Informasi Sistem)"| lolcat
+echo -e "2.)usernew (Membuat Akun SSH & OpenVPN) 8.)info (Menampilkan Informasi VPS)"| lolcat
 echo -e "3.)trial (Membuat Akun Trial)           9.)reboot (Reboot VPS)"| lolcat
 echo -e "4.)hapus (Menghapus Akun SSH & OpenVPN) 10.)speedtest (Speedtest VPS)"| lolcat
 echo -e "5.)cek (Cek User Login)                 11.)exit (Keluar Dari VPS)"| lolcat
